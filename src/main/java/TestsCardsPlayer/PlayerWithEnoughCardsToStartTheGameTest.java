f217441f381f8a3328d963f4d8267db3176c670f
Franco Mendoza <mendozafranco2105@gmail.com>
2025-05-30 16:07:31 -0300

    link update

---
+++ src/main/java/TestsCardsPlayer/PlayerWithEnoughCardsToStartTheGameTest.java
@@ -0,0 +1,12 @@
+package TestsCardsPlayer;
+
+
+import org.junit.Test;
+
+public class PlayerWithEnoughCardsToStartTheGameTest {
+
+    @Test
+    public void testPlayerWithEnoughCardsToStartTheGame() {
+
+    }
+}
